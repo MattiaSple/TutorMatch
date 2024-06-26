b79b056818a600a82b8dfea7cbba253e48075934
Mattia Splendiani <m.splendiani02@gmail.com>
2024-06-26 17:46:12 +0200

    ok

--- app/src/main/java/com/example/tutormatch/db/Calendario.kt
+++ app/src/main/java/com/example/tutormatch/db/Calendario.kt
@@ -7,6 +7,6 @@ import java.util.Date
 @Entity(tableName = "calendario")
 data class Calendario(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val data: Date,
+    val data: Date?,
     val stato_pren: Boolean,
     val email_prof: String)
