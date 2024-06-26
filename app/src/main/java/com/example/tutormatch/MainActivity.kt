e93043a40a253dcffae9d15bc125aa55dabe98d9
FilErc <S1107630@studenti.univpm.it>
2024-06-26 12:58:14 +0200

    layout

--- app/src/main/java/com/example/tutormatch/MainActivity.kt
+++ app/src/main/java/com/example/tutormatch/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.tutormatch
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_homestudente)
+        setContentView(R.layout.activity_main)
+        val tutor = findViewById<Button>(R.id.creaAccountTutor)
+        tutor.setOnClickListener{
+            intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
