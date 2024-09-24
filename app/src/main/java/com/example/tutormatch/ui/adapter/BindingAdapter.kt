d4876651e61ae9bf1e80a63d51fc6ef74e8370ec
FilErc <S1107630@studenti.univpm.it>
2024-09-24 11:39:35 +0200

    FROCio

--- app/src/main/java/com/example/tutormatch/ui/adapter/BindingAdapter.kt
+++ app/src/main/java/com/example/tutormatch/ui/adapter/BindingAdapter.kt
@@ -6,11 +6,18 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 @BindingAdapter("timestamp")
-fun setTimestamp(textView: TextView, timestamp: Long) {
-    val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
-    val date = Date(timestamp)
-    textView.text = sdf.format(date)
+fun setTimestamp(textView: TextView, timestamp: Long?) {
+    // Verifica se il timestamp non è nullo
+    if (timestamp != null && timestamp > 0) {
+        val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
+        val date = Date(timestamp)
+        textView.text = sdf.format(date)
+    } else {
+        // Se il timestamp non è valido o è nullo, mostra un testo di fallback
+        textView.text = "Data non disponibile"
+    }
 }
+
 @BindingAdapter("participants")
 fun bindParticipants(textView: TextView, participants: List<String>?) {
     participants?.let {
