5857c451c9da9f0f0c741591865c8753665202de
FilErc <S1107630@studenti.univpm.it>
2025-01-21 13:33:03 +0100

    ci

--- app/src/main/java/com/example/tutormatch/data/model/Chat.kt
+++ app/src/main/java/com/example/tutormatch/data/model/Chat.kt
@@ -8,7 +8,7 @@ data class Chat(
     var id: String = "",
     val participants: List<String> = emptyList(), // Email dei partecipanti
     val participantsNames: List<String> = emptyList(), // Nomi completi dei partecipanti
-    val subject: String = "", // Materia di discussione
+    val subject: String = "", // Materia di discussione,
     val lastMessage: Message? = null, // Ultimo messaggio inviato,
     val messages: Map<String, Message> = emptyMap() // Messaggi della chat
 ) {
