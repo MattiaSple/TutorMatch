e25f83ccfd1da17649dc9cc76a42542d50e4fb6b
FilErc <S1107630@studenti.univpm.it>
2025-01-21 13:32:33 +0100

    ci

--- app/src/main/java/com/example/tutormatch/data/model/Chat.kt
+++ app/src/main/java/com/example/tutormatch/data/model/Chat.kt
@@ -9,7 +9,7 @@ data class Chat(
     val participants: List<String> = emptyList(), // Email dei partecipanti
     val participantsNames: List<String> = emptyList(), // Nomi completi dei partecipanti
     val subject: String = "", // Materia di discussione
-    val lastMessage: Message? = null, // Ultimo messaggio inviato
+    val lastMessage: Message? = null, // Ultimo messaggio inviato,
     val messages: Map<String, Message> = emptyMap() // Messaggi della chat
 ) {
     // Proprieta calcolata per verificare se ci sono messaggi non letti per l'utente attuale
